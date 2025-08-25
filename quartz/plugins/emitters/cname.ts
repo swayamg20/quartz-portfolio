a17679fbd66f8ef28b529148cbb5bace3585ae7d
Swayam Gupta <swayam.gupta@travenues.com>
2025-08-26 00:23:10 +0530

    Quartz sync: Aug 26, 2025, 12:23 AM

--- quartz/plugins/emitters/cname.ts
+++ quartz/plugins/emitters/cname.ts
@@ -8,22 +8,22 @@ export function extractDomainFromBaseUrl(baseUrl: string) {
   return url.hostname
 }
 
-export const CNAME: QuartzEmitterPlugin = () => ({
-  name: "CNAME",
-  async emit({ argv, cfg }) {
-    if (!cfg.configuration.baseUrl) {
-      console.warn(
-        styleText("yellow", "CNAME emitter requires `baseUrl` to be set in your configuration"),
-      )
-      return []
-    }
-    const path = joinSegments(argv.output, "CNAME")
-    const content = extractDomainFromBaseUrl(cfg.configuration.baseUrl)
-    if (!content) {
-      return []
-    }
-    await fs.promises.writeFile(path, content)
-    return [path] as FilePath[]
-  },
-  async *partialEmit() {},
-})
+// export const CNAME: QuartzEmitterPlugin = () => ({
+//   name: "CNAME",
+//   async emit({ argv, cfg }) {
+//     if (!cfg.configuration.baseUrl) {
+//       console.warn(
+//         styleText("yellow", "CNAME emitter requires `baseUrl` to be set in your configuration"),
+//       )
+//       return []
+//     }
+//     const path = joinSegments(argv.output, "CNAME")
+//     const content = extractDomainFromBaseUrl(cfg.configuration.baseUrl)
+//     if (!content) {
+//       return []
+//     }
+//     await fs.promises.writeFile(path, content)
+//     return [path] as FilePath[]
+//   },
+//   async *partialEmit() {},
+// })
