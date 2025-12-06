7d282dbbfc23b6fd62fff22c573c24ec89a88fdd
Swayam Gupta <gupta.swayam123@gmail.com>
2025-12-07 01:19:26 +0530

    Add GitHub Pages deployment workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "swayamg20.github.io/quartz-portfolio",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
